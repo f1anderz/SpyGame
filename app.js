674ca0a09570fa6dee7ec0414deabd58fb4d150a
Stas Stepaniuk <ssttaass0415@gmail.com>
2024-02-20 12:37:53 +0200

    Added error handling

--- app.js
+++ app.js
@@ -11,4 +11,19 @@ app.use(morgan('dev'));
 app.use('/users', userRoutes);
 app.use('/games', gameRoutes);
 
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        error:{
+            status: false,
+            message: err.message
+        }
+    })
+});
+
 module.exports = app;

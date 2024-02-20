bbf398b1ccf82e5c996ab936fde1ef389eca1fbe
Stas Stepaniuk <ssttaass0415@gmail.com>
2024-02-20 12:32:27 +0200

    Added nodemon, morgan and dotenv packages for easier development and environment variables

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
+require('dotenv').config();
 const app = express();
 
 const userRoutes = require("./api/routes/user");
 const gameRoutes = require("./api/routes/game");
+
+app.use(morgan('dev'));
 
 app.use('/users', userRoutes);
 app.use('/games', gameRoutes);

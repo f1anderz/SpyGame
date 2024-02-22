4ef291a75ddb2864d5344d7e690434d6177214ff
Stas Stepaniuk <ssttaass0415@gmail.com>
2024-02-22 15:12:00 +0200

    Added model and base requests for games

--- api/models/game.js
+++ api/models/game.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const gameSchema = new mongoose.Schema({
-    _id: mongoose.Types.ObjectId,
-
+    _id: mongoose.Schema.Types.ObjectId,
+    users: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }],
+    locationsCollection: {
+        type: mongoose.Types.ObjectId,
+        ref: "LocationsCollection",
+        required: true
+    }
 });
 
-module.exports =  mongoose.model('Game', gameSchema);
+module.exports = mongoose.model('Game', gameSchema);

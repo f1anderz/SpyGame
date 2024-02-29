d860cbd8965bef0a28043160dfc1f160f55e93ee
Stas Stepaniuk <ssttaass0415@gmail.com>
2024-02-29 18:41:39 +0200

    added user api requests and vuex getters, mutations and actions

--- src/api/user.js
+++ src/api/user.js
@@ -1,11 +1,28 @@
+import axios from 'axios';
+import api from '@/api';
+
 export default {
-    registerUser(user){
-
-    },
-    loginUser(user){
-
-    },
-    getUser(id){
-
+    async register(registerData) {
+        return await axios.post('http://localhost:3333/users', {
+            username: registerData.username, login: registerData.login, password: registerData.password
+        }, {
+            headers: {
+                'Content-Type': 'application/json', 'x-api-key': api.key
+            }
+        });
+    }, async login(loginData) {
+        return await axios.post('http://localhost:3333/users/login', {
+            login: loginData.login, password: loginData.password
+        }, {
+            headers: {
+                'Content-Type': 'application/json', 'x-api-key': api.key
+            }
+        });
+    }, async getUser(id) {
+        return await axios.get(`http://localhost:3333/users/${id}`, {
+            headers: {
+                'x-api-key': api.key
+            }
+        });
     }
 }
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,26 +1,41 @@
+import userAPI from '@/api/user.js';
+import {inject} from 'vue';
+
 const state = {
-    username: '',
-    isInGame: false
+    id: '', username: '', isInGame: false
 };
 
-const getters = {}
-
-const actions = {}
+const getters = {
+    isUserLoggedIn(state) {
+        return state.id.length > 0;
+    }
+}
 
 const mutations = {
-    joinGame(state) {
+    setID(state, userID) {
+        state.id = userID;
+    },setUsername(state, username) {
+        state.username = username;
+    }, joinGame(state) {
         state.isInGame = true;
-    }
-    ,
-    leaveGame(state) {
+    }, leaveGame(state) {
         state.isInGame = false;
     }
 }
 
+const actions = {
+    loginUser(context, loginData) {
+        return userAPI.login(loginData);
+    }, async setUser(context, userID) {
+        const cookies = inject('$cookies');
+        cookies.set('userID', userID, "30d");
+        let response = await userAPI.getUser(userID);
+        context.commit('setID', response.data.user._id);
+        context.commit('setUsername', state.username = response.data.user.username);
+        return context.state.username;
+    }
+}
+
 export default {
-    namespaced: true,
-    state,
-    getters,
-    actions,
-    mutations
+    namespaced: true, state, getters, actions, mutations
 }

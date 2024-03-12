85b112a4419618b6fdf6d8c4d9b8db49ae364509
Stas Stepaniuk <ssttaass0415@gmail.com>
2024-03-12 14:23:47 +0200

    Corrected naming in select list component

--- src/components/UI/SpySelectList.vue
+++ src/components/UI/SpySelectList.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="spy-dropdown">
-    <div class="spy-dropdown-header">{{ props.dropdownOptions.header }}</div>
-    <div class="spy-dropdown-options">
-      <transition-group name="spy-dropdown-options">
-        <spy-checkbox v-for="option in props.dropdownOptions.list" :key="option._id" :option="option"
+  <div class="spy-select-list">
+    <div class="spy-select-list-header">{{ props.selectListOptions.header }}</div>
+    <div class="spy-select-list-options">
+      <transition-group name="spy-select-list-options">
+        <spy-checkbox v-for="option in props.selectListOptions.list" :key="option._id" :option="option"
                       @selected="(value)=>{emit('optionSelect', value)}"/>
       </transition-group>
     </div>
@@ -20,14 +20,14 @@ import SpyCheckbox from '@/components/UI/SpyCheckbox.vue';
 const emit = defineEmits(['optionSelect']);
 
 const props = defineProps({
-  dropdownOptions: Object
+  selectListOptions: Object
 });
 </script>
 
 <style scoped lang="scss">
 @use '@/assets/scss/style';
 
-.spy-dropdown {
+.spy-select-list {
   position: relative;
   display: flex;
   flex-direction: column;
